04fd253845434b03d438c7fc47fe8d41370c8c6b
anna-zai1 <125779710+anna-zai1@users.noreply.github.com>
2024-10-21 10:51:25 -0400

    Update ctagger_wrapper.m

--- ctagger_wrapper.m
+++ ctagger_wrapper.m
@@ -1,22 +1,54 @@
 function selectedTags = ctagger_wrapper(eventStruct)
-    % Load the CTAGGER Java package or JAR file
     javaaddpath('CTAGGER/CTAGGER.jar'); 
     
     import CTagger;       
     import HedValidator;   
     
     % Initialize the CTAGGER GUI with event data
-    % Instantiate the CTagger class and pass the event data
     ctagger = javaObject('CTagger', false, false, '', '', true);
     
-    % Pass the eventStruct for annotation
     ctagger.loadEventData(eventStruct);
     
-    % Open the CTAGGER GUI for user interaction
     ctagger.displayGUI();
     
-    % Retrieve the selected tags from CTAGGER after GUI is closed
     selectedTags = ctagger.getSelectedTags(); 
+end
+
+function save_tags_to_brainstorm(selectedTags, sMat)
+    DataMat = in_bst_data(sMat.FileName);
     
-    % Return the selected tags to MATLAB for further processing
+    % Add the selected tags to the event structure in the DataMat
+    for i = 1:length(selectedTags)
+        DataMat.Events(i).label = selectedTags{i};
+    end
+
+    bst_save(sMat.FileName, DataMat, 'v6');
+end
+
+function eventStruct = brainstorm_get_event_struct(sInputs)
+    DataMat = in_bst_data(sInputs.FileName);
+
+    % Get the event structure from the data
+    events = DataMat.F.events;
+    
+    % Create a structure compatible with CTAGGER
+    for i = 1:length(events)
+        eventStruct(i).eventType = events(i).label;
+        eventStruct(i).times = events(i).times;
+    end
+end
+
+% This function will be called when the user selects the CTAGGER process
+function mymenu = AddCtaggerMenu()
+    % Create a new menu in the Brainstorm GUI
+    mymenu = uimenu(gui_brainstorm('figure'), 'Label', 'CTAGGER Plugin');
+    
+    % Add a submenu to the CTAGGER menu
+    uimenu(mymenu, 'Label', 'Tag Events', 'Callback', @RunCtaggerProcess);
+end
+
+% Callback function for running the CTAGGER process
+function RunCtaggerProcess(hObject, eventdata)
+    % Run the CTAGGER Brainstorm process
+    process_ctagger_tag_events();
 end
