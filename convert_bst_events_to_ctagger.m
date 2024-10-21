4588e4fbc7c822ef1c9ee46b9fdc7346ffd88f9b
anna-zai1 <125779710+anna-zai1@users.noreply.github.com>
2024-10-21 10:50:15 -0400

    Delete convert_bst_events_to_ctagger.m

--- convert_bst_events_to_ctagger.m
+++
@@ -1,8 +0,0 @@
-function eventStruct = convert_bst_events_to_ctagger(events)
-    % Convert Brainstorm events into a structure compatible with CTAGGER
-    
-    for i = 1:length(events)
-        eventStruct(i).eventType = events(i).label;  
-        eventStruct(i).times = events(i).times;  
-    end
-end
