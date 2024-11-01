65a994b56ac0e43a455dbf5f504003b5a698d3eb
anna-zai1 <125779710+anna-zai1@users.noreply.github.com>
2024-11-01 10:48:15 -0400

    Update ctagger_wrapper.m

--- ctagger_wrapper.m
+++ ctagger_wrapper.m
@@ -1,23 +1,21 @@
 function selectedTags = ctagger_wrapper(eventStruct)
-    javaaddpath('CTAGGER/CTAGGER.jar'); 
+    javaaddpath('CTagger.jar'); 
     
     import CTagger;       
     import HedValidator;   
     
-    % Initialize the CTAGGER GUI with event data
     ctagger = javaObject('CTagger', false, false, '', '', true);
-    
     ctagger.loadEventData(eventStruct);
     
     ctagger.displayGUI();
     
-    selectedTags = ctagger.getSelectedTags(); 
+    selectedTags = ctagger.getSelectedTags();
 end
+
 
 function save_tags_to_brainstorm(selectedTags, sMat)
     DataMat = in_bst_data(sMat.FileName);
     
-    % Add the selected tags to the event structure in the DataMat
     for i = 1:length(selectedTags)
         DataMat.Events(i).label = selectedTags{i};
     end
@@ -28,26 +26,20 @@ end
 function eventStruct = brainstorm_get_event_struct(sInputs)
     DataMat = in_bst_data(sInputs.FileName);
 
-    % Get the event structure from the data
     events = DataMat.F.events;
     
-    % Create a structure compatible with CTAGGER
     for i = 1:length(events)
         eventStruct(i).eventType = events(i).label;
         eventStruct(i).times = events(i).times;
     end
 end
 
-% This function will be called when the user selects the CTAGGER process
 function mymenu = AddCtaggerMenu()
-    % Create a new menu in the Brainstorm GUI
     mymenu = uimenu(gui_brainstorm('figure'), 'Label', 'CTAGGER Plugin');
     
-    % Add a submenu to the CTAGGER menu
     uimenu(mymenu, 'Label', 'Tag Events', 'Callback', @RunCtaggerProcess);
 end
 
-% Callback function for running the CTAGGER process
 function RunCtaggerProcess(hObject, eventdata)
     % Run the CTAGGER Brainstorm process
     process_ctagger_tag_events();
