2f45d95d2a4f86a45342602a5dea71e5e99f28d8
anna-zai1 <125779710+anna-zai1@users.noreply.github.com>
2024-11-04 20:24:02 -0500

    Update brainstorm_plugin.m

--- brainstorm_plugin.m
+++ brainstorm_plugin.m
@@ -1,18 +1,13 @@
 function ctagger_plugin_install()
-    % Path to the CTAGGER folder
-    pluginPath = '/path/to/CTAGGER';
+    pluginPath = 'CTAGGER';
 
-    % Check if CTAGGER is already in the path
     if ~exist(pluginPath, 'dir')
         error('CTAGGER Plugin folder does not exist: %s', pluginPath);
     end
 
-    % Add the CTAGGER jar file to Java classpath
     javaaddpath(fullfile(pluginPath, 'CTAGGER.jar'));
 
-    % Add the CTAGGER folder to MATLAB path
     addpath(pluginPath);
 
-    % Display success message
     disp('CTAGGER plugin successfully installed!');
 end
