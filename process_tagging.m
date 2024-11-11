bd38e2f46bc88bfd4560ae648e18ce17ae92857c
anna-zai1 <125779710+anna-zai1@users.noreply.github.com>
2024-11-11 08:29:22 -0500

    Update process_tagging.m

--- process_tagging.m
+++ process_tagging.m
@@ -3,21 +3,26 @@ function sProcess = process_add_hed_tags()
     sProcess.FileTag     = '';
     sProcess.Category    = 'Custom';
     sProcess.SubGroup    = 'Event';
-    sProcess.Index       = 201; % Arbitrary index
+    sProcess.Index       = 201;  % Arbitrary index
 
     sProcess.InputTypes  = {'data', 'raw'};
     sProcess.OutputTypes = {'data', 'raw'};
     sProcess.nInputs     = 1;
     sProcess.nMinFiles   = 1;
 
-    sProcess.options.description.Comment = 'Launch CTAGGER GUI to add HED tags';
+    sProcess.options.description.Comment = 'Launch CTAGGER to select HED tags';
     sProcess.options.description.Type    = 'label';
 end
 
 function OutputFiles = Run(sProcess, sInputs)
+    outputPath = 'selected_tags.json';
+
     for iFile = 1:numel(sInputs)
         DataMat = in_bst_data(sInputs(iFile).FileName);
-        DataMat.F.events = OpenCTAGGER(DataMat.F.events);
+        
+        LaunchCTAGGERWithExport(DataMat.F.events, outputPath);
+
+        DataMat.F.events = UpdateEventsWithHEDTags(DataMat.F.events, outputPath);
         
         OutputFiles{iFile} = bst_process('GetOutputFile', sProcess, sInputs(iFile).FileName);
         save(OutputFiles{iFile}, '-struct', 'DataMat');
